48addbac88b968f643bd773072742c9cca4236aa
iency <pwnzjake@gmail.com>
2022-07-15 18:51:30 +0300

    commit

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,12 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import Book from '../components/ui/Book';
 import Price from '../components/ui/Price';
 import Rating from '../components/ui/Rating';
 
-const BookInfo = ({ books }) => {
-  const { id } = (useParams);
+const BookInfo = ({ books, addToCart, cart }) => {
+  const { id } = useParams();
   const book = books.find(book => +book.id === +id);
+
+  function addBookToCart(book) {
+    addToCart(book);
+  }
+
+  function bookExistsOnCart() {
+    return cart.find((book) => +book.id === +id)
+  }
 
   return (
     <div id="books__body">
@@ -42,7 +51,15 @@ const BookInfo = ({ books }) => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque architecto saepe excepturi blanditiis omnis magnam id necessitatibus repudiandae mollitia nulla laudantium, sint, fugiat quas nam! Inventore odit explicabo adipisci beatae!
                   </p>
                 </div>
-                <button className='btn'>Add to Cart</button>
+                {bookExistsOnCart() ? (
+                  <Link to={`/cart`} className="book__link">
+                    <button className="btn">Checkout</button>
+                  </Link>
+                ) : (
+                  <button className='btn' onClick={() => addBookToCart(book)}>
+                    Add to Cart
+                  </button>
+                )}
               </div>
             </div>
           </div>
@@ -54,6 +71,14 @@ const BookInfo = ({ books }) => {
               <h2 className="book__selected--title--top">
                 Recommended Books
               </h2>
+            </div>
+            <div className="books">
+              {
+                books
+                  .filter((book) => book.rating === 5 && +book.id !== +id)
+                  .slice(0, 4)
+                  .map(book => <Book book={book} key={book.id} />)
+              }
             </div>
           </div>
         </div>
